ea39b81d66c25d31913ba453c6533359f2c66b84
Ioritz_Berrospe <iberrospeez21ba@ikzubirimanteo.com>
2023-03-16 08:55:53 +0100

    formularioa bukatzen es increible bro

--- 3. EBAL MANEX/datuaksartu.js
+++ 3. EBAL MANEX/datuaksartu.js
@@ -1,4 +1,4 @@
-async function ekarriOrdua() {
-    var data = await (await fetch('http://127.0.0.1:8000/items/4?q=4')).json()
-    document.getElementById("ordua").innerHTML= data.item_id;
+async function sartuUrteak() {
+    var data = await (await fetch('http://zubiriapi.duckdns.org:8000/insert/insert%20into%20urteak%20values/taldea1')).json()
+    document.getElementById("urteaksartuta").innerHTML= 'Jarritako datuak sartu dira!';
 }
